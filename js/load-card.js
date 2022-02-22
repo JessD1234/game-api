01fc35a01872c9bd2205ab4604ea5eb1cc3a5be2
jessicaduckworth <duckworthj@byui.edu>
2022-02-22 10:09:03 -0700

    Adding button functionality test 1

--- js/load-card.js
+++ js/load-card.js
@@ -1,12 +1,11 @@
-//import storage from "./storage.js";
 
 document.querySelector("#newGame").onclick = loadCharacters;
 
 function loadCharacters () {
-
-    //document.getElementById("characterGrid").innerHTML = 'Start a new game:';
+    document.querySelector('#characterGrid').innerHTML = '';
 
     const characterList = getCharacterList();
+
 
     characterList.forEach(character => {
         createCharacterCard(character);
@@ -14,46 +13,78 @@ function loadCharacters () {
 }
 
 function createCharacterCard(character) {
+
     //Create individual character div
     const characterDiv = document.createElement('div');
     characterDiv.classList.add("CharacterCard");
     characterDiv.setAttribute('id', character.id);
 
+    //Create character information
+    const infoDiv = document.createElement('div');
+    const name = document.createElement('p');
+    name.innerText = character.Name;
+    const district = document.createElement('p');
+    district.innerText = "District: " + character.District;
+    infoDiv.appendChild(name);
+    infoDiv.appendChild(district);
+
+
     //Create health section
     console.log(character);
     console.log(character.Health);
-    const healthValue = document.createElement('p')
-    healthValue.innerText(character.Health);
+    const healthValue = document.createElement('p');
+    healthValue.classList.add("characterHealth");
+    healthValue.innerText = "Health Points: " + character.Health;
     
 
     const minusOneButton = document.createElement('button');
-    minusOneButton.innerText("Minus 1 health");
+    const buttonOneMinusId = "MinusOneButton" + character.ID;
+    minusOneButton.setAttribute('id', buttonOneMinusId);
+    minusOneButton.classList.add("buttonMinusOne");
+    minusOneButton.innerText = "Minus 1 health";
     minusOneButton.onclick = minusHealth;
 
     const minusFiveButton = document.createElement('button');
-    minusFiveButton.innerText("Minus 5 health");
+    const buttonFiveMinusId = "MinusFiveButton" + character.ID;
+    minusFiveButton.setAttribute('id', buttonFiveMinusId);
+    minusFiveButton.classList.add("buttonMinusFive");
+    minusFiveButton.innerText = "Minus 5 health";
     minusFiveButton.onclick = minusHealth;
 
     const minusTenButton = document.createElement('button');
-    minusTenButton.innerText("Minus 10 health");
+    const buttonTenMinusId = "MinusTenButton" + character.ID;
+    minusTenButton.setAttribute('id', buttonTenMinusId);
+    minusTenButton.classList.add("buttonMinusTen");
+    minusTenButton.innerText = "Minus 10 health";
     minusTenButton.onclick = minusHealth;
 
     const plusOneButton = document.createElement('button');
-    plusOneButton.innerText("Plus 1 health");
-    plusOneButton.onclick = plusHealth;
+    const buttonOnePlusId = "PlusOneButton" + character.ID;
+    plusOneButton.setAttribute('id', buttonOnePlusId);
+    plusOneButton.classList.add("buttonPlusOne");
+    plusOneButton.innerText = "Plus 1 health";
+   // plusOneButton.onclick = plusHealth;
 
     const plusFiveButton = document.createElement('button');
-    plusFiveButton.innerText("Plus 5 health");
-    plusFiveButton.onclick = plusHealth;
+    const buttonFivePlusId = "PlusFiveButton" + character.ID;
+    plusFiveButton.setAttribute('id', buttonFivePlusId);
+    plusFiveButton.classList.add("buttonPlusFive");
+    plusFiveButton.innerText = "Plus 5 health";
+   // plusFiveButton.onclick = plusHealth;
 
     const plusTenButton = document.createElement('button');
-    plusTenButton.innerText("Plus 10 health");
-    plusTenButton.onclick = plusHealth;
+    const buttonTenPlusId = "PlusTenButton" + character.ID;
+    plusTenButton.setAttribute('id', buttonTenPlusId);
+    plusTenButton.classList.add("buttonPlusTen");
+    plusTenButton.innerText = "Plus 10 health";
+   // plusTenButton.onclick = plusHealth;
 
     /* if (character.health == 0) {
-        //Add overlay? What? How to indicate dead?
+        // Add overlay? What? How to indicate dead?
+        // Debating creating a separate list at the bottom/top of the board listing the dead characters.
     } */
 
+    characterDiv.appendChild(infoDiv);
     characterDiv.appendChild(healthValue);
     characterDiv.appendChild(minusOneButton);
     characterDiv.appendChild(minusFiveButton);
@@ -61,10 +92,25 @@ function createCharacterCard(character) {
     characterDiv.appendChild(plusOneButton);
     characterDiv.appendChild(plusFiveButton);
     characterDiv.appendChild(plusTenButton);
-
+    document.querySelector('#characterGrid').appendChild(characterDiv);
 
 }
 
+function minusHealth() {
+    const characterList = localStorage.getItem("characterList");
+    const button = e.currentTarget;
+    const buttonName = button.classList;
+    console.log(buttonName);
+
+    //I just realized this won't work, because being able to build case statements for each of the unique button items would take forever/be unrealistic. Considering there's 24 tributes and 6 buttons each, I'd be here a while. So I was thinking of adding a class as well. Select the button by the class, and then also pull in the character information based off the button ID? We need a way to save this back to the character, so maybe just set the button ID to the character's ID? Look at how I edited to-do items or the budget items for reference.
+   /*  switch (buttonId) {
+        case 
+    } */
+
+}
+
+
+//Storage functions
 function getCharacterList () {
     const characterList = localStorage.getItem("characterList");
 
@@ -77,4 +123,11 @@ function getCharacterList () {
     }
 
     return characterArray;
+}
+
+function saveCharacter (character) {
+    const characterList = getCharacterList();
+    characterList.push(character);
+    localStorage.setItem("characterList", JSON.stringify(characterList));
+
 }
