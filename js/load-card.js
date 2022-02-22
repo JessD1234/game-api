60c7215050e99cb60dddd8fd1c004d2ce43675f4
jessicaduckworth <duckworthje1@yahoo.com>
2022-02-21 21:35:13 -0700

    Testing

--- js/load-card.js
+++ js/load-card.js
@@ -1,4 +1,4 @@
-import storage from "./storage.js";
+//import storage from "./storage.js";
 
 document.querySelector("#newGame").onclick = loadCharacters;
 
@@ -6,7 +6,8 @@ function loadCharacters () {
 
     document.getElementById("characterGrid").innerHTML = '';
 
-    const characterList = storage.getCharacterList();
+    //const characterList = storage.getCharacterList();
+    const characterList = [{"ID": "1M", "Name": "Garrett", "District": 1, "Health": 24}, {"ID": "11F", "Name": "Shia", "District": 11, "Health": 24}];
 
     characterList.forEach(character => {
         createCharacterCard(character);
